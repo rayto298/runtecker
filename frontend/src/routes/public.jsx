e0fa95427c2c01f327b077e37545b3f6feb9cab6
MaTTalv001 <pomeranian1981@gmail.com>
2024-03-12 23:59:31 +0900

    RUNTEQ生以外がログインしようとした時は専用ページにリダイレクトする

--- frontend/src/routes/public.jsx
+++ frontend/src/routes/public.jsx
@@ -1,21 +1,22 @@
-import { Suspense } from "react"
-import { MainLayout } from "views/layouts/main_layout"
-import { _Loading } from "views/layouts/components/_loading"
-import { Outlet } from "react-router-dom"
-import { RoutePath } from "config/route_path"
-import { CurriculumsIndex } from "views/curriculums"
-import { EventsIndex } from "views/events"
-import { EventsShow } from "views/events/show"
-import { JobMeasuresIndex } from "views/job_measures"
-import { JobMeasuresShow } from "views/job_measures/show"
-import { RecruitsIndex } from "views/recruits"
-import { RecruitsShow } from "views/recruits/show"
-import { UsersIndex } from "views/users"
-import { UsersNew } from "views/users/new"
-import { UsersShow } from "views/users/show"
-import { StaticPagesIndex } from "views/static_pages"
-import { StaticPagesNotfound } from "views/static_pages/not_found"
-import { UserSessionsNew } from "views/user_sessions/new"
+import { Suspense } from "react";
+import { MainLayout } from "views/layouts/main_layout";
+import { _Loading } from "views/layouts/components/_loading";
+import { Outlet } from "react-router-dom";
+import { RoutePath } from "config/route_path";
+import { CurriculumsIndex } from "views/curriculums";
+import { EventsIndex } from "views/events";
+import { EventsShow } from "views/events/show";
+import { JobMeasuresIndex } from "views/job_measures";
+import { JobMeasuresShow } from "views/job_measures/show";
+import { RecruitsIndex } from "views/recruits";
+import { RecruitsShow } from "views/recruits/show";
+import { UsersIndex } from "views/users";
+import { UsersNew } from "views/users/new";
+import { UsersShow } from "views/users/show";
+import { StaticPagesIndex } from "views/static_pages";
+import { StaticPagesNotfound } from "views/static_pages/not_found";
+import { UserSessionsNew } from "views/user_sessions/new";
+import { StaticPagesNotRuntecker } from "views/static_pages/not_runtecker";
 
 const App = () => {
   return (
@@ -23,8 +24,8 @@ const App = () => {
       <Suspense fallback={<_Loading />} />
       <Outlet />
     </MainLayout>
-  )
-}
+  );
+};
 
 //  TODO : ユーザー認証確定するまでルーティングを暫定的に設定しています。
 export const PUBLIC_ROUTES = [
@@ -45,6 +46,10 @@ export const PUBLIC_ROUTES = [
       { path: RoutePath.UsersNew.path, element: <UsersNew /> },
       { path: RoutePath.UsersShow.path(), element: <UsersShow /> },
       { path: RoutePath.NotFound.path, element: <StaticPagesNotfound /> },
-    ]
-  }
-]
+      {
+        path: RoutePath.NotRuntecker.path,
+        element: <StaticPagesNotRuntecker />,
+      },
+    ],
+  },
+];
