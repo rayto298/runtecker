91c01b8ec9ffaceb7ddfe2ad3bbcba9e33cf8fb0
伊藤 一希 <kzkio114@gmail.com>
2024-03-25 01:16:58 +0900

    アナリティス確認用？

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { AuthProvider } from "providers/auth";
 import ReactGA from 'react-ga4'; // ReactGA をインポート
 
+
 export const App = () => {
-  useEffect(() => {
-    // GA4 のトラッキングID で初期化
-    ReactGA.initialize('G-F7P97W3Z2VZ'); // 実際のトラッキングIDに置き換えてください
-  }, []);
+    useEffect(() => {
+      ReactGA.initialize('G-F7P97W3Z2VZ'); // GA4のトラッキングIDで初期化
+    }, []);
 
   return (
     <AuthProvider>
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { useAuth } from "../providers/auth"; 
-import { PROTECTED_ROUTES, PUBLIC_ROUTES } from "./routes";
+import { useAuth } from "../providers/auth";
+import { PROTECTED_ROUTES } from "./protected";
+import { PUBLIC_ROUTES } from "./public";
 import { useRoutes } from "react-router-dom";
-import ReactGA from 'react-ga4';
+import ReactGA from 'react-ga4'; // ReactGA をインポート
 
 export const AppRoutes = () => {
   const location = useLocation();
-  const { auth } = useAuth();
-  const routes = auth ? PROTECTED_ROUTES : PUBLIC_ROUTES;
+  const { auth } = useAuth(); // 認証状態をuseAuthフックから取得
+  const routes = auth ? PROTECTED_ROUTES : PUBLIC_ROUTES; // 認証状態に応じてルートを切り替え
   const element = useRoutes([...routes]);
 
   useEffect(() => {
