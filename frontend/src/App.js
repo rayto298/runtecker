06c9c9998481ab4bd94fa739e4fe358a9d65b18b
伊藤 一希 <kzkio114@gmail.com>
2024-03-25 00:34:58 +0900

    アナリティス

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { AuthProvider } from "providers/auth";
+import ReactGA from 'react-ga4'; // ReactGA をインポート
 
 export const App = () => {
-  
+  useEffect(() => {
+    // GA4 のトラッキングID で初期化
+    ReactGA.initialize('G-F7P97W3Z2VZ'); // 実際のトラッキングIDに置き換えてください
+  }, []);
+
   return (
     <AuthProvider>
       <BrowserRouter>
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,11 +1,21 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { useAuth } from "../providers/auth"; 
-import { PROTECTED_ROUTES } from "./protected";
-import { PUBLIC_ROUTES } from "./public";
+import { PROTECTED_ROUTES, PUBLIC_ROUTES } from "./routes";
 import { useRoutes } from "react-router-dom";
+import ReactGA from 'react-ga4';
 
 export const AppRoutes = () => {
-  const { auth } = useAuth(); // 認証状態をuseAuthフックから取得
-  const routes = auth ? PROTECTED_ROUTES : PUBLIC_ROUTES; // 認証状態に応じてルートを切り替え
+  const location = useLocation();
+  const { auth } = useAuth();
+  const routes = auth ? PROTECTED_ROUTES : PUBLIC_ROUTES;
   const element = useRoutes([...routes]);
+
+  useEffect(() => {
+    // ページ遷移があるたびにページビューイベントをGA4に送信
+    const pagePath = location.pathname + location.search;
+    ReactGA.send({ hitType: "pageview", page: pagePath });
+  }, [location]);
+
   return <>{element}</>;
 };
