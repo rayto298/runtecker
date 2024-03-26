14a83bb1633646b2077195e2c3f9b0b8f4f35372
Kanae-Nishina <karubona.ra247@gmail.com>
2024-03-26 12:29:47 +0900

    ログイン状態に応じたページ遷移

--- frontend/src/views/static_pages/index.jsx
+++ frontend/src/views/static_pages/index.jsx
@@ -1,8 +1,16 @@
-
-
+import { RoutePath } from "config/route_path";
+import { useAuth } from "providers/auth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const StaticPagesIndex = () => {
-  return (
-    <div>index</div>
-  )
+  const { currentUser } = useAuth();
+  const router = useNavigate();
+  useEffect(() => {
+    if (currentUser) {
+      router(RoutePath.Users.path);
+    } else {
+      router(RoutePath.Login.path);
+    }
+  }, [currentUser]);
 }
